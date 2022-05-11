64ff09cd8fe5b0d66da059a1f2ced69f58eda59c
Zulfadhli Zakari <zufadhlizakari@gmail.com>
2022-05-11 11:48:36 +0800

    bug: fix image component

--- components/cloud-image.jsx
+++ components/cloud-image.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 
-export default function CloudImage({ src, alt, width, height }) {
+export default function CloudImage({ src, alt }) {
   return (
-    <figure className='relative py-4 [break-inside:avoid]'>
+    <figure
+      className={`relative w-full aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8 [break-inside:avoid] `}
+    >
       <Image
         src={src}
         alt={alt}
-        width={width}
-        height={height}
         layout='fill'
         objectFit='cover'
         placeholder='blur'
